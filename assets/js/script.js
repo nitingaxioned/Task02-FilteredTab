bbb895e0d2d9837cf0fe7692368b0c253741a61e
Nitin <niting@axioned.com>
2021-10-21 16:34:57 +0530

    js edited

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,25 @@
 // variables declerations
-
-// for node lists
 var imgNodeList = document.querySelectorAll(".img-list li");
 var btnNodeList = document.querySelectorAll(".filter-btn");
-// for btn
-var all_btn = document.querySelector(".all");
-var lawn_btn = document.querySelector(".lawn-care");
-var garden_btn = document.querySelector(".garden-care");
-var plant_btn = document.querySelector(".planting");
 
 // Event Listners
-
-all_btn.addEventListener("click", function(){
-    imgNodeList.forEach(function(val){
-        val.classList.remove("hide-me");
+btnNodeList.forEach(function(val, index){
+    val.addEventListener("click", function(){
+        btnNodeList.forEach(function(value){ value.classList.remove("selected"); });
+        val.classList.add("selected");
+        imgNodeList.forEach(function(value){
+            value.classList.remove("hide-me");
+            switch(index){
+                case 1:
+                    value.classList.contains("lawn") || value.classList.add("hide-me");
+                    break;
+                case 2:
+                    value.classList.contains("garden") || value.classList.add("hide-me");
+                    break;
+                case 3:
+                    value.classList.contains("plant") || value.classList.add("hide-me");
+                    break;
+            }
+        });
     });
-    btnNodeList.forEach(function(val){
-        val.classList.remove("selected");
-    });
-    all_btn.classList.add("selected");
 });
-
-lawn_btn.addEventListener("click", function(){
-    imgNodeList.forEach(function(val){
-        if(val.classList.contains("lawn"))
-            val.classList.remove("hide-me");
-        else
-            val.classList.add("hide-me");
-    });
-    btnNodeList.forEach(function(val){
-        val.classList.remove("selected");
-    });
-    lawn_btn.classList.add("selected");
-});
-
-garden_btn.addEventListener("click", function(){
-    imgNodeList.forEach(function(val){
-        if(val.classList.contains("garden"))
-            val.classList.remove("hide-me");
-        else
-            val.classList.add("hide-me");
-    });
-    btnNodeList.forEach(function(val){
-        val.classList.remove("selected");
-    });
-    garden_btn.classList.add("selected");
-});
-
-plant_btn.addEventListener("click", function(){
-    imgNodeList.forEach(function(val){
-        if(val.classList.contains("plant"))
-            val.classList.remove("hide-me");
-        else
-            val.classList.add("hide-me");
-    });
-    btnNodeList.forEach(function(val){
-        val.classList.remove("selected");
-    });
-    plant_btn.classList.add("selected");
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
